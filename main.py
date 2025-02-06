a7814c0e853dd7690a5b59666e53c4e727cb9d85
hewscode <mattthomas303@gmail.com>
2025-02-06 13:17:28 +1100

    major fixes, dont go back.

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import random
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = "2-sided", "4-sided", "6-sided", "8-sided", 
-"10-sided", "20-sided", "100-sided",
-
+dice_type = 2
 
 def select_face_option(event):
     global dice_type  # use global var named dice_type
@@ -19,8 +17,8 @@ def select_face_option(event):
 def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-
-    document.querySelector("div#roll-history").innerHTML = "<p id='something'> + str()>"
+    result = dice.dice_roll(dice_type)
+    document.querySelector("div#roll-history").innerHTML = "<p id='something'>" + str(result) +"</p>"
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
